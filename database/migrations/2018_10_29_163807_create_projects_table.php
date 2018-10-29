47e99ecc59d68cf10f48bf0e0b64af86514e8f23
Shahzad Ahmad <shazin.kh043@gmail.com>
2018-10-29 23:33:16 +0500

    add migration table

--- database/migrations/2018_10_29_163807_create_projects_table.php
+++ database/migrations/2018_10_29_163807_create_projects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,6 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
@@ -29,8 +20,6 @@ class CreateUsersTable extends Migration
             $table->date('start_date');
             $table->date('end_date');
             $table->text('description');
-            $table->foreign('onwer_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('user_assigned')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -42,6 +31,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('projects', function (Blueprint $table) {
+            //
+        });
     }
 }
---
+++ database/migrations/2018_10_29_170331_create_project_notes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_notes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id')->unsigned();
+            $table
+                ->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->onDelete('CASCADE')
+            ;
+            $table->string('title');
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('project_notes', function (Blueprint $table) {
+            //
+        });
+    }
+}
